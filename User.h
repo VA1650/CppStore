40045d8ab7d27b4f10533834568087ef8055443e
VA1650 <57103544+VA1650@users.noreply.github.com>
2022-04-09 12:29:15 +0300

    Add files via upload

---
+++ User.h
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class User {
+private:
+	string name;
+	string adress;
+	double balance;
+	double spent;
+
+public:
+	User(string name, string adress, double balance, double spent) {
+		this->name = name;
+		this->adress = adress;
+		this->balance = balance;
+		this->spent = spent;
+	}
+
+	void ReduceBalance(double price) {
+		balance -= price;
+		spent += price;
+	}
+
+
+	void Print() {
+		cout << name << "\t" << adress << "\t" << balance << "\t" << spent << endl;
+	}
+
+};
